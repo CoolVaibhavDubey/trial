8f3c700ffbe8e463256519275e969886f42186ec
Vaibhav Dubey <vaibhav.dubey.iet@gmail.com>
2023-12-21 11:02:37 +0530

    added button

---
+++ src/Ankit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ankit {
+    public static void main (String []args){
+//        System.out.println("Hello World!");
+        Scanner scn =new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int sum = n+m;
+        System.out.println(sum);
+        //hello
+    }
+}
